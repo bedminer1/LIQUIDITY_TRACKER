6f5f24158601b2168cb3e1c2ec5b4004ef4d2172
bedminer1 <alexander.seah.min.rui@gmail.com>
2025-01-05 01:27:41 +0800

    idk

--- frontend/src/routes/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import { AppShell } from "@skeletonlabs/skeleton"
-</script>
-
-<div class="h-full w-1/2 flex justify-center items-center">
-	<p class="text-3xl">Test, this sucks lol</p>
-</div>
